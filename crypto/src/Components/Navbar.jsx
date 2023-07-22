1dd8f794de76b41c2215b34bcfda501acf17b101
George Githinji <zangigeorge742gmail.com>
2023-07-23 02:24:17 +0300

    first commit

---
+++ crypto/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import logo from "../"
+
+const Navbar = () => {
+  return (
+
+    <div>
+    <nav className="justify-between flex gap-4 justify-items-center">
+      <ul>Home</ul>
+      <ul>Pricing</ul>
+      <ul>About Us</ul>
+      <ul>Market</ul>
+    </nav>
+    </div>
+
+  );
+}
+
+export default Navbar;
